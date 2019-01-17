f01cd72df4d6853b2806a0e1d6c5b2f2b6985948
Noel Barrios <noel.barrios@optum.com>
2019-01-16 18:05:07 -0600

    rewrite the same User Interface using only C#

--- StackLayoutExample1/MainPage.xaml.cs
+++ StackLayoutExample1/MainPage.xaml.cs
@@ -12,6 +12,39 @@ namespace StackLayoutExample1
         public MainPage()
         {
             InitializeComponent();
+
+            var red = new Label
+            {
+                Text = "Stop",
+                BackgroundColor = Color.Red,
+                FontSize = 20,
+                WidthRequest = 100
+            };
+
+            var yellow = new Label
+            {
+                Text = "Slow down",
+                BackgroundColor = Color.Yellow,
+                FontSize = 20,
+                WidthRequest = 100
+            };
+
+            var green = new Label
+            {
+                Text = "Go",
+                BackgroundColor = Color.Green,
+                FontSize = 20,
+                WidthRequest = 200
+            };
+
+            Content = new StackLayout
+            {
+                Spacing = 10,
+                VerticalOptions = LayoutOptions.End,
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.Start,
+                Children = { red, yellow, green }
+            };
         }
     }
 }
